c1066e65fd30dd2191d017204b1892d4715c381a
Jerlyn Rodriguez <jerlynrodriguez14@gmail.com>
2026-02-20 15:30:20 -0400

    Initial code for TSS.Api

---
+++ Controllers/NominaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TSS.Api.Models;
+
+namespace TSS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NominaController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult RecibirNomina([FromBody] NominaDto nomina)
+        {
+            return Ok(new
+            {
+                mensaje = "Nomina recibida correctamente",
+                asiento = nomina.NoAsiento
+            });
+        }
+    }
+
+}
---
+++ Models/NominaDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TSS.Api.Models
+{
+    public class NominaDto
+    {
+        public string NoAsiento { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string CuentaContable { get; set; }
+        public string TipoMovimiento { get; set; }
+        public decimal Monto { get; set; }
+        public string Asignacion { get; set; }
+    }
+}
